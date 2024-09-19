5e13332f326a03b509805b2427fc84667f328b12
Jonathan Tan <960166+kuyajon@users.noreply.github.com>
2024-09-19 21:56:33 +0800

    Generated empty vite + react frontend

---
+++ src/main/java/com/kuyajon/learningportal/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.kuyajon.learningportal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/assets/**", "/index.html", "/").permitAll()  // Allow access to static resources including index.html
+                                .anyRequest().authenticated() // Require authentication for other requests
+                )
+                .formLogin(withDefaults()) // Configure login form
+                .logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+}
