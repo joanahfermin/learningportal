de163236a8ae98901c44a2aef4fc5787a31c6327
Jonathan O. Tan <jonathan.tan@itiviti.com>
2024-09-21 16:23:41 +0800

    Create courses dev data
    Change-Id: I71b4eb6e45dfabbde13ad332451214dada9b6f02

--- src/main/java/com/kuyajon/learningportal/config/DBBootstrap.java
+++ src/main/java/com/kuyajon/learningportal/config/DBBootstrap.java
@@ -6,10 +6,22 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.kuyajon.learningportal.repository.course.CourseRepository;
+import com.kuyajon.learningportal.repository.course.LessonRepository;
+import com.kuyajon.learningportal.repository.course.QuestionRepository;
+import com.kuyajon.learningportal.repository.course.TestRepository;
+import com.kuyajon.learningportal.repository.course.TopicRepository;
+import com.kuyajon.learningportal.model.course.AnswerChoice;
+import com.kuyajon.learningportal.model.course.Course;
+import com.kuyajon.learningportal.model.course.Lesson;
+import com.kuyajon.learningportal.model.course.Question;
+import com.kuyajon.learningportal.model.course.Test;
+import com.kuyajon.learningportal.model.course.Topic;
 import com.kuyajon.learningportal.model.sys.ERole;
 import com.kuyajon.learningportal.model.sys.User;
 import com.kuyajon.learningportal.repository.sys.UserRepository;
 import com.kuyajon.learningportal.service.SecurityService;
+
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +41,18 @@ public class DBBootstrap {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private TestRepository testRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
@@ -44,6 +68,7 @@ public class DBBootstrap {
     private void createInitialDataForDev() {
         LOG.info("Creating Initial Data for DEV");
         createDevUsers();
+        createDevCourses();
         LOG.info("Creating Initial Data for DEV - Done");
     }
 
@@ -53,10 +78,60 @@ public class DBBootstrap {
     }
 
     private void createDevUsers() {
-        if (userRepository.count()==0) {
+        if (userRepository.count() == 0) {
             securityService.createUser("admin", "admin", ERole.ADMIN);
             securityService.createUser("stu", "stu", ERole.STUDENT);
         }
     }
 
+    private void createDevCourses() {
+        if (courseRepository.count() == 0) {
+            for (int c = 0; c < 5; c++) {
+                Course course = new Course();
+                course.setName("Course " + c);
+                course.setDescription("The quick brown course.");
+                courseRepository.save(course);
+                for (int l = 0; l < 5; l++) {
+                    Lesson lesson = new Lesson();
+                    lesson.setName("Course " + c + " Lesson " + l);
+                    lesson.setDescription("The quick brown lesson.");
+                    lesson.setSortOrder(l);
+                    lesson.setCourse(course);
+                    lessonRepository.save(lesson);
+                    createTestObject(lesson, null);
+
+                    for (int t = 0; t < 3; t++) {
+                        Topic topic = new Topic();
+                        topic.setName("Topic " + t);
+                        topic.setContent("**Welcome to topic!**\nHello *world*");
+                        topic.setSortOrder(t);
+                        topic.setLesson(lesson);
+                        topicRepository.save(topic);
+                        createTestObject(null, topic);
+                    }    
+                }
+            }
+        }
+    }
+
+    private void createTestObject(Lesson lesson, Topic topic) {
+        Test test = new Test();
+        test.setName("Test " + UUID.randomUUID());
+        test.setLesson(lesson);
+        test.setTopic(topic);
+        testRepository.save(test);
+        
+        for (int q=0; q<5; q++) {
+            Question question = new Question();
+            question.setQuestionText("What is?");
+            question.setChoiceA("Is it A");
+            question.setChoiceB("Is it B");
+            question.setChoiceC("Is it C");
+            question.setChoiceD("Is it D");
+            question.setAnswer(AnswerChoice.B);
+            question.setSolution("Here is how to solve it");
+            question.setTest(test);
+            questionRepository.save(question);
+        }
+    }
 }
