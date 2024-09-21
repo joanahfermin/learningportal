5151bc809c0f1db46f755ece0e59ee3d54e644c8
joanah <ferminjoanah@gmail.com>
2024-09-21 13:20:02 +0800

    -fix wrong parameter in UserRoleRepository class -added basic crud functions in service (Client, Course, systeService)

--- src/main/java/com/kuyajon/learningportal/service/ClientService.java
+++ src/main/java/com/kuyajon/learningportal/service/ClientService.java
@@ -1,7 +1,95 @@
 package com.kuyajon.learningportal.service;
 
+import com.kuyajon.learningportal.model.client.*;
+import com.kuyajon.learningportal.repository.client.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private ClientGroupRepository clientGroupRepository;
+
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    private LessonProgressRepository lessonProgressRepository;
+
+    @Autowired
+    private TopicProgressRepository topicProgressRepository;
+
+
+
+    //Retrieve client by id.
+    public Client getClientByID(Long id){
+        return clientRepository.findById(id).get();
+    }
+
+    public ClientGroup getClientGroupByID(Long id){
+        return clientGroupRepository.findById(id).get();
+    }
+
+    public Enrollment getEnrollmentByID(Long id){
+        return enrollmentRepository.findById(id).get();
+    }
+
+    public LessonProgress getLessonProgressByID(Long id){
+        return lessonProgressRepository.findById(id).get();
+    }
+
+    public TopicProgress getTopicProgressByID(Long id){
+        return topicProgressRepository.findById(id).get();
+    }
+
+
+
+    //Save or update client obj.
+    public Client saveOrUpdateClient(Client client){
+        return clientRepository.save(client);
+    }
+
+    public ClientGroup saveOrUpdateClientGroup(ClientGroup clientGroup){
+        return clientGroupRepository.save(clientGroup);
+    }
+
+    public Enrollment saveOrUpdateEnrollment(Enrollment enrollment){
+        return enrollmentRepository.save(enrollment);
+    }
+
+    public LessonProgress saveOrUpdateLessonProgress(LessonProgress enrollment){
+        return lessonProgressRepository.save(enrollment);
+    }
+
+    public TopicProgress saveOrUpdateTopicProgress(TopicProgress topicProgress){
+        return topicProgressRepository.save(topicProgress);
+    }
+
+
+
+    //Delete client obj.
+    public void deleteClientById(Long id){
+        clientRepository.deleteById(id);
+    }
+
+    public void deleteClientGroupById(Long id){
+        clientGroupRepository.deleteById(id);
+    }
+
+    public void deleteEnrollmentById(Long id){
+        enrollmentRepository.deleteById(id);
+    }
+
+    public void deleteLessonProgressById(Long id){
+        lessonProgressRepository.deleteById(id);
+    }
+
+    public void deleteTopicProgressById(Long id){
+        topicProgressRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/kuyajon/learningportal/service/CourseService.java
+++ src/main/java/com/kuyajon/learningportal/service/CourseService.java
@@ -1,7 +1,95 @@
 package com.kuyajon.learningportal.service;
 
+import com.kuyajon.learningportal.model.client.*;
+import com.kuyajon.learningportal.model.course.*;
+import com.kuyajon.learningportal.repository.course.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private TestRepository testRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+
+
+    //Retrieve course by id.
+    public Course getCourseByID(Long id){
+        return courseRepository.findById(id).get();
+    }
+
+    public Lesson getLessonByID(Long id){
+        return lessonRepository.findById(id).get();
+    }
+
+    public Question getQuestionByID(Long id){
+        return questionRepository.findById(id).get();
+    }
+
+    public Test getTestByID(Long id){
+        return testRepository.findById(id).get();
+    }
+
+    public Topic getTopicID(Long id){
+        return topicRepository.findById(id).get();
+    }
+
+
+
+    //Save or update course obj.
+    public Course saveOrUpdateCourse(Course course){
+        return courseRepository.save(course);
+    }
+
+    public Lesson saveOrUpdateLesson(Lesson lesson){
+        return lessonRepository.save(lesson);
+    }
+
+    public Question saveOrUpdateQuestion(Question question){
+        return questionRepository.save(question);
+    }
+
+    public Test saveOrUpdateTest(Test test){
+        return testRepository.save(test);
+    }
+
+    public Topic saveOrUpdateTopic(Topic topic){
+        return topicRepository.save(topic);
+    }
+
+
+
+    //Delete client obj.
+    public void deleteCourseById(Long id){
+        courseRepository.deleteById(id);
+    }
+
+    public void deleteLessonById(Long id){
+        lessonRepository.deleteById(id);
+    }
+
+    public void deleteQuestionById(Long id){
+        questionRepository.deleteById(id);
+    }
+
+    public void deleteTestById(Long id){
+        testRepository.deleteById(id);
+    }
+
+    public void deleteTopicById(Long id){
+        topicRepository.deleteById(id);
+    }
 }
